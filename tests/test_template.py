59e78d8ac138d93b266ba021ca1091ee69556738
Michael Klatt <mdklatt@alumni.ou.edu>
2019-06-03 22:25:32 -0500

    Refactored template test script

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,34 +1,31 @@
-""" Test the cpp-lib Cookiecutter template.
+""" Test the Cookiecutter template.
 
 A template project is created in a temporary directory, the library and its
 test suite are built, and the test suite is executed.
 
 """
-from contextlib import contextmanager
-from cookiecutter.main import cookiecutter
-from json import load
-from os import chdir
-from os.path import abspath
-from os.path import dirname
-from os.path import join
+from json import loads
+from pathlib import Path
 from shlex import split
 from subprocess import check_call
 from tempfile import TemporaryDirectory
 
+from cookiecutter.main import cookiecutter
 
-def main():
+
+def main() -> int:
     """ Execute the test.
     
     """
-    template = dirname(dirname(abspath(__file__)))
-    defaults = load(open(join(template, "cookiecutter.json")))
+    template = Path(__file__).resolve().parents[1]
+    defaults = loads(template.joinpath("cookiecutter.json").read_text())
     with TemporaryDirectory() as tmpdir:
-        chdir(tmpdir)
-        cookiecutter(template, no_input=True)
-        chdir(defaults["project_slug"])
-        check_call(split("cmake -DCMAKE_BUILD_TYPE=Debug"))
-        check_call(split("cmake --build ."))
-        check_call(split(join("test", "test_hello")))
+        cookiecutter(str(template), no_input=True, output_dir=tmpdir)
+        name = defaults["project_slug"]
+        cwd = Path(tmpdir) / name
+        for args in "-DCMAKE_BUILD_TYPE=Debug", "--build .":
+            check_call(split(f"cmake {args:s}"), cwd=cwd)
+        check_call(split(f"test/test_{name:s}"), cwd=cwd)
     return 0
     
     
