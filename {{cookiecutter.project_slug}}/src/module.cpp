bf88e3fc015a5f43406273c37a1cf44156b73ed8
Michael Klatt <mdklatt@alumni.ou.edu>
2023-07-20 01:38:33 -0500

    Tweaked code style in template

--- {{cookiecutter.project_slug}}/src/module.cpp
+++ {{cookiecutter.project_slug}}/src/module.cpp
@@ -1,17 +1,16 @@
-/// Implementation of the sample library module.
-///
+/**
+ * Implementation of the sample library module.
+ */
 #include "{{ cookiecutter.lib_name }}/module.hpp"
 
 using {{ cookiecutter.lib_name }}::SampleClass;
 
 
-int SampleClass::add(int x) const
-{
+int SampleClass::add(int x) const {
     return {{ cookiecutter.lib_name }}::add(num, x);
 }
 
 
-int {{ cookiecutter.lib_name }}::add(int x, int y)
-{
+int {{ cookiecutter.lib_name }}::add(int x, int y) {
     return x + y;
 }
--- {{cookiecutter.project_slug}}/tests/unit/test_module.cpp
+++ {{cookiecutter.project_slug}}/tests/unit/test_module.cpp
@@ -1,17 +1,18 @@
-/// Test suite for the sample module.
-///
-/// Link all test files with the `gtest_main` library to create a command-line 
-/// test runner.
-///
-#include <gtest/gtest.h>
+/**
+ * Test suite for the sample module.
+ *
+ * Link all test files with the `gtest_main` library to create a command-line
+ * test runner.
+ */
 #include "{{ cookiecutter.lib_name }}/module.hpp"
+#include <gtest/gtest.h>
 
 using namespace {{ cookiecutter.lib_name }};
 
 
-/// Test a sample function call.
-///
-TEST(add, test)
-{
+/**
+ * Test a sample function call.
+ */
+TEST(add, test) {
     ASSERT_EQ(3, add(1, 2));
 }
